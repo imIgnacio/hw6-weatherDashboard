b7ae05f831d329ff603c6b1fb84272dc1e54ca48
Ignacio Diaz <nachodiaz99@gmail.com>
2021-09-25 17:23:38 +1000

    Current temp finished, when you change city it does successfully. TODO 5-day forecast, localStorage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,7 +13,9 @@ function getCityWeather() {
 
     var cityName = cityNameInput.value; //Get input from user
     var linkRequest = urlAPI + cityName + "&appid=" + APIKey + "&units=metric";
-    console.log(linkRequest);
+
+    //Clear previous content
+    clearValues();
 
     fetch(linkRequest)
     .then(function(response){
@@ -21,10 +23,13 @@ function getCityWeather() {
     })
     .then(function(data){
 
+        //Display date next to city name
+        var dateCalendar = new Date(data.dt*1000);
+
         //Display name of the city
-        var cityName = document.createElement("h3");
-        cityName.textContent = data.name + ", " + data.sys.country;
-        citySummary.prepend(cityName);
+        var cityNameAndDate = document.createElement("h3");
+        cityNameAndDate.textContent = data.name + ", " + data.sys.country + ". " + dateCalendar.toLocaleDateString();
+        citySummary.prepend(cityNameAndDate);
 
         //Get Icon from API
         var weatherIconCode = data.weather[0].icon;
@@ -33,7 +38,7 @@ function getCityWeather() {
         //Display icon next to name
         var weatherIcon = document.createElement("img");
         weatherIcon.setAttribute("src",iconURL);
-        citySummary.appendChild(weatherIcon);
+        citySummary.appendChild(weatherIcon);        
 
         //Create elements to show weather info
         var cityTemp = document.createElement("li");
@@ -54,9 +59,12 @@ function getCityWeather() {
         var lon = data.coord.lon;
         getCityUV(lat, lon);
 
+        //Get 5-day forecast
+        getForecast();
     })
 }
 
+//Function to get UV index using API
 function getCityUV(lat, lon){
     var oneCallURL = oneCallAPI + lat + "&lon=" + lon + "&appid=" + APIKey;
 
@@ -66,9 +74,58 @@ function getCityUV(lat, lon){
     })
     .then(function(data){
         var indexUV = document.createElement("li");
-        indexUV.textContent = "UV Index: " + data.current.uvi;
+        indexUV.textContent = "UV Index: ";
+        var UVpar = document.createElement("p");
+        UVpar.textContent = data.current.uvi;
+
+        setUVIndexColor(UVpar);
+
+        indexUV.appendChild(UVpar);
         tempInfo.appendChild(indexUV);
+
+        getForecast(data);
     })
 };
+
+//Set UV Index color
+function setUVIndexColor(UVpar) {
+    if(UVpar.textContent < 2){
+        UVpar.style.backgroundColor = "green";
+    }else if(UVpar.textContent < 5){
+        UVpar.style.backgroundColor = "yellow";
+    }else if(UVpar.textContent < 7){
+        UVpar.style.backgroundColor = "orange";
+    }else if(UVpar.textContent < 10){
+        UVpar.style.backgroundColor = "red";
+    }else{
+        UVpar.style.backgroundColor = "purple";
+    }
+}
+
+//Get 5-day forecast TODO
+function getForecast(lat, lon) {
+    return;
+}
+
+//Remove children from city-summary and temperature-info
+function clearValues() {
+    if(!citySummary.firstChild){
+        return;
+    }else{
+        while(citySummary.firstChild){
+            citySummary.removeChild(citySummary.firstChild);
+        }
+    }
+
+    if(!tempInfo.firstChild){
+        return;
+    }else{
+        while(tempInfo.firstChild){
+            tempInfo.removeChild(tempInfo.firstChild);
+        }
+    }
+}
+
+
 
 submitButton.addEventListener("click", getCityWeather);
