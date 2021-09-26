2697c7d8298d19d3846c656ad1804fb01a69b39a
Ignacio Diaz <nachodiaz99@gmail.com>
2021-09-26 14:02:26 +1000

    App working fine, local storage finished. App loads all forecast from any city saved on recent search

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,10 +4,12 @@ const oneCallAPI = "https://api.openweathermap.org/data/2.5/onecall?lat=";
 
 var cityNameInput = document.querySelector(".search-city");
 var submitButton = document.querySelector("#button");
+var searchHistory = document.querySelector(".recent-search");
 var cityWeather = document.querySelector(".weather-info");
 var citySummary = document.querySelector(".city-summary");
 var tempInfo = document.querySelector(".temperature-info");
 var cardsArray = document.querySelectorAll(".card");
+var citiesArray = [];
 
 function getCityWeather() {
 
@@ -16,7 +18,10 @@ function getCityWeather() {
         alert("Please input a valid city");
         return;
     }
+    citiesArray.push(cityName); //Add city to recently views
+    localStorage.setItem("cities",JSON.stringify(citiesArray)); //Save city on local storage
 
+    displayLastCity();
     var linkRequest = urlAPI + cityName + "&appid=" + APIKey + "&units=metric";
 
     //Clear previous content
@@ -166,4 +171,79 @@ function clearValues() {
     })
 }
 
+//Add last city to search history
+function displayLastCity() {
+    var lastCityAux = citiesArray[citiesArray.length -1];
+    var lastCity = lastCityAux[0].toUpperCase() + lastCityAux.substring(1);
+    var cityButton = document.createElement("input");
+    cityButton.setAttribute("id","button");
+    cityButton.setAttribute("type","submit");
+    cityButton.setAttribute("value",lastCity);
+
+    //Add button to search history
+    searchHistory.appendChild(cityButton);
+}
+
+function getWeatherViewed(lastCity){
+    var linkRequest = urlAPI + lastCity + "&appid=" + APIKey + "&units=metric";
+
+    //Clear previous content
+    clearValues();
+
+    fetch(linkRequest)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+
+        //Display date next to city name
+        var dateCalendar = new Date(data.dt*1000);
+
+        //Display name of the city
+        var cityNameAndDate = document.createElement("h3");
+        cityNameAndDate.textContent = data.name + ", " + data.sys.country + ". " + dateCalendar.toLocaleDateString();
+        citySummary.prepend(cityNameAndDate);
+
+        //Get Icon from API
+        var weatherIconCode = data.weather[0].icon;
+        var iconURL = "http://openweathermap.org/img/wn/" + weatherIconCode + "@2x.png";
+
+        //Display icon next to name
+        var weatherIcon = document.createElement("img");
+        weatherIcon.setAttribute("src",iconURL);
+        citySummary.appendChild(weatherIcon);        
+
+        //Create elements to show weather info
+        var cityTemp = document.createElement("li");
+        var cityWind = document.createElement("li");
+        var cityHumidity = document.createElement("li");
+        var cityUV = document.createElement("li");
+
+        //Display weather info
+        cityTemp.textContent = "Temperature: " + data.main.temp + "°C";
+        tempInfo.appendChild(cityTemp);
+        cityWind.textContent = "Wind: " + data.wind.speed + "km/h";
+        tempInfo.appendChild(cityWind);
+        cityHumidity.textContent = "Humidity: " + data.main.humidity + "%";
+        tempInfo.appendChild(cityHumidity);
+
+        //Get UV index from One Call API
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+        getCityUV(lat, lon);
+    })
+}
+
+//Display Sydney for Default
+getWeatherViewed("sydney");
+
+//Listener to display user city
 submitButton.addEventListener("click", getCityWeather);
+
+//Listener to display city viewed before
+searchHistory.addEventListener('click', function(e) {
+    e = e || window.event;
+    var target = e.target;
+    var city = target.getAttribute("value");
+    getWeatherViewed(city);
+}, false);
